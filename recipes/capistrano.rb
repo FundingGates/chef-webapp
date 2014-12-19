27629461a0167bc1b42f69f7a6fde11f1a5adcf9
Matthew Scott <scott@fundinggates.com>
2014-12-18 23:20:15 -0600

    Create an empty/dummy release if there is no current release
    This is useful for bootstrapping.

--- recipes/capistrano.rb
+++ recipes/capistrano.rb
@@ -18,16 +18,13 @@ directory "#{app.releases_path}/empty" do
   owner app.user.name
   group app.user.group
   mode 0775
+  not_if "test -f #{app.current_path}"
 end
 
 directory "#{app.releases_path}/empty/config" do
   owner app.user.name
   group app.user.group
   mode 0700
-end
-
-link app.current_path do
-  to "#{app.releases_path}/empty"
   not_if "test -f #{app.current_path}"
 end
 
@@ -47,4 +44,26 @@ directory "#{app.shared_path}/config" do
   owner app.user.name
   group app.user.group
   mode 0700
+end
+
+directory "#{app.shared_path}/assets" do
+  owner app.user.name
+  group app.user.group
+  mode 0775
+end
+
+# Create an empty manifest.json to start out with before anything's deployed.
+# It doesn't appear to be actually used by Business-Portal though,
+# as ember-cli takes care of asset building.
+file "#{app.shared_path}/assets/manifest.json" do
+  owner app.user.name
+  group app.user.group
+  mode 0664
+  not_if "test -f #{app.current_path}"
+end
+
+# Finally, link the current release to the dummy 'empty' release.
+link app.current_path do
+  to "#{app.releases_path}/empty"
+  not_if "test -f #{app.current_path}"
 end
