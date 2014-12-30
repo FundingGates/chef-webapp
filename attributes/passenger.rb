66b50534c26671cbf4ef4759167ef1fae6ed61fa
Matthew Scott <scott@fundinggates.com>
2014-12-30 12:59:34 -0600

    Use version 4.0.30 of passenger, which fixes Date header generation
    cf. https://github.com/phusion/passenger/compare/release-4.0.29...release-4.0.30
    cf. https://github.com/phusion/passenger/pull/93

--- attributes/passenger.rb
+++ attributes/passenger.rb
@@ -4,7 +4,7 @@ include_attribute 'webapp::nginx' # Need nginx attributes to set configure flags
 # We're going to install passenger into the default / system ruby
 default[:webapp][:passenger][:ruby] = node[:global_ruby_version]
 default[:webapp][:passenger][:common_pkgs] = %w( libssl-dev zlib1g-dev libcurl4-openssl-dev )
-default[:webapp][:passenger][:version] = '4.0.29'
+default[:webapp][:passenger][:version] = '4.0.30'
 default[:webapp][:passenger][:root_path] = "/usr/local/rvm/gems/#{node[:webapp][:passenger][:ruby]}/gems/passenger-#{node[:webapp][:passenger][:version]}"
 default[:webapp][:passenger][:ruby_wrapper] = "/usr/local/rvm/rubies/#{node[:webapp][:passenger][:ruby]}/bin/ruby"
 
